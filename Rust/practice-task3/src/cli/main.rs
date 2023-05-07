9195e715a5f50300d37449b9bdaba9557dac2a65
Huang_Yuhan <huang_yuhan@sjtu.edu.cn>
2023-05-08 00:23:07 +0800

    done

--- Rust/practice-task3/src/cli/main.rs
+++ Rust/practice-task3/src/cli/main.rs
@@ -8,18 +8,27 @@ use search::{WordSearch, SearchWord};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut arg_parser=ArgParser::new();
-    for i in args  {
-        let parseResult=arg_parser.add_argument(i);
-        match parseResult {
-            Ok(_)=>continue,
-            Err(err)=>
-            {
-                panic!("Wrong Arg")
-        }
-        }
-    }   
+    match arg_parser.add_argument(args[1].clone()){
+        Err(err)=>
+        panic!("error"),
+        Ok(_)=>{}
+    }
+
+    match arg_parser.add_argument(args[2].clone()){
+        Err(err)=>
+        panic!("error"),
+        Ok(_)=>{}
+    }
+
+    match arg_parser.add_argument(args[3].clone()){
+        Err(err)=>
+        panic!("error"),
+        Ok(_)=>{}
+    }
 
     let config=arg_parser.parse();
+
+    println!("{}", config.input_file);
 
     let content=fs::read_to_string(&config.input_file)
     .expect("read file error");
@@ -29,5 +38,8 @@ fn main() {
     search_word.add_content(&content);
 
     let count=search_word.get_count(&config.word);
+
+    fs::write(&config.output_file, count.to_string())
+    .expect("write to file error");
 
 }
